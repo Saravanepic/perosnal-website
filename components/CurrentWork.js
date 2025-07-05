7812ca53b3bd27be140a3f55fbc4073de333e6f3
Saravan <saravan1996.krishna@gmail.com>
2025-07-05 12:07:02 +0530

    Add New

--- components/CurrentWork.js
+++ components/CurrentWork.js
@@ -46,6 +46,57 @@ const CurrentWork = ({ noTitle = false }) => {
         <div className="container">
           <div className="portfolio_in">
             <ul>
+              
+               <li>
+                <div className="list_inner">
+                  <div className="image">
+                    <img src="img/portfolio/IB.png" alt={4} />
+                  </div>
+                  <div className="title_holder">
+                    <div className="left">
+                      <span className="tag">Shopify Website</span>
+                      <h3>
+                        Ice Barrel
+                      </h3>
+                    </div>
+                    <div className="right">
+                    <Link href="https://icebarrel.com/" target="_blank">
+                        <img
+                          className="sm_svg"
+                          src="img/svg/down_arrow.svg"
+                          alt={1}
+                        />
+                      </Link>
+                    </div>
+                  </div>
+                </div>
+              </li>
+
+              <li>
+                <div className="list_inner">
+                  <div className="image">
+                    <img src="img/portfolio/Timba.png" alt={4} />
+                  </div>
+                  <div className="title_holder">
+                    <div className="left">
+                      <span className="tag">Shopify Website</span>
+                      <h3>
+                      Timba
+                      </h3>
+                    </div>
+                    <div className="right">
+                    <Link href="https://bytimba.com/" target="_blank">
+                        <img
+                          className="sm_svg"
+                          src="img/svg/down_arrow.svg"
+                          alt={1}
+                        />
+                      </Link>
+                    </div>
+                  </div>
+                </div>
+              </li>
+              
               <li>
                 <div className="list_inner">
                   <div className="image">
@@ -70,30 +121,7 @@ const CurrentWork = ({ noTitle = false }) => {
                   </div>
                 </div>
               </li>
-              <li>
-                <div className="list_inner">
-                  <div className="image">
-                    <img src="img/portfolio/4.jpg" alt={4} />
-                  </div>
-                  <div className="title_holder">
-                    <div className="left">
-                    <span className="tag">Front End Development</span>
-                      <h3>
-                          pimCloud Application
-                      </h3>
-                    </div>
-                    <div className="right">
-                      <Link href="">
-                        <img
-                          className="sm_svg"
-                          src="img/svg/down_arrow.svg"
-                          alt={1}
-                        />
-                      </Link>
-                    </div>
-                  </div>
-                </div>
-              </li>
+              
              
 
               <li>
@@ -121,55 +149,7 @@ const CurrentWork = ({ noTitle = false }) => {
                 </div>
               </li>
 
-              <li>
-                <div className="list_inner">
-                  <div className="image">
-                    <img src="img/portfolio/5.jpg" alt={4} />
-                  </div>
-                  <div className="title_holder">
-                    <div className="left">
-                      <span className="tag">Shopify Website</span>
-                      <h3>
-                        Ice Barrel
-                      </h3>
-                    </div>
-                    <div className="right">
-                    <Link href="https://icebarrel.com/" target="_blank">
-                        <img
-                          className="sm_svg"
-                          src="img/svg/down_arrow.svg"
-                          alt={1}
-                        />
-                      </Link>
-                    </div>
-                  </div>
-                </div>
-              </li>
-
-              <li>
-                <div className="list_inner">
-                  <div className="image">
-                    <img src="img/portfolio/6.jpg" alt={4} />
-                  </div>
-                  <div className="title_holder">
-                    <div className="left">
-                      <span className="tag">Wordpress Website</span>
-                      <h3>
-                      Digital ConsultingHaus LLC
-                      </h3>
-                    </div>
-                    <div className="right">
-                    <Link href="https://dimgrey-horse-249812.hostingersite.com/" target="_blank">
-                        <img
-                          className="sm_svg"
-                          src="img/svg/down_arrow.svg"
-                          alt={1}
-                        />
-                      </Link>
-                    </div>
-                  </div>
-                </div>
-              </li>
+             
            
             </ul>
           </div>
