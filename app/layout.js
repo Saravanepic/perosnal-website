4e24b19c51dec299a6fee6e5cf11f2fa92c9685e
Saravan <saravan1996.krishna@gmail.com>
2025-07-08 01:01:56 +0530

    Graph Image Update

--- app/layout.js
+++ app/layout.js
@@ -17,7 +17,7 @@ export const metadata = {
     siteName: "Saravan - Freelancer",
     images: [
       {
-        url: "https://cdn.shopify.com/s/files/1/0580/4346/5911/files/MobileLogo.png?v=1751916524",
+        url: "https://cdn.shopify.com/s/files/1/0580/4346/5911/files/Open_Graph.png?v=1751916677",
         width: 1200,
         height: 630,
         alt: "Saravan - Freelancer",
