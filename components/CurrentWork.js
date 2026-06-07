51d45e7feb6566eb869e998d5f5fab79cb48aa16
Saravan <40654542+Saravanepic@users.noreply.github.com>
2026-06-07 19:50:28 +0530

    Manthan pic update

--- components/CurrentWork.js
+++ components/CurrentWork.js
@@ -258,23 +258,23 @@ const CurrentWork = ({ noTitle = false }) => {
               <li>
                 <div className="list_inner">
                   <div className="image">
-                    <img src="img/portfolio/5.jpg" alt={4} />
+                    <img src="img/portfolio/maanthan.png" alt={4} />
                   </div>
                   <div className="title_holder">
                     <div className="left">
-                      <span className="tag">SASS App</span>
+                      <span className="tag">SASS Portal - WIP</span>
                       <h3>
                         Custom Job Portal
                       </h3>
                     </div>
                     <div className="right">
-                    <Link href="" target="_blank">
+                    {/* <Link href="" target="_blank">
                         <img
                           className="sm_svg"
                           src="img/svg/down_arrow.svg"
                           alt={1}
                         />
-                      </Link>
+                      </Link> */}
                     </div>
                   </div>
                 </div>
